336b5013b11adcd9176391012e9df1d17d5ff942
DevBoiAgru <79085233+DevBoiAgru@users.noreply.github.com>
2025-07-17 22:40:26 +0530

    Original domain cors

--- api/numberlike.go
+++ api/numberlike.go
@@ -92,7 +92,6 @@ func handleGet(w http.ResponseWriter, r *http.Request) {
 
 func handleOptions(w http.ResponseWriter, _ *http.Request) {
 	// Set CORS headers
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Access-Control-Max-Age", "86400") // 1 day
