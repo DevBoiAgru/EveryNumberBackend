25f038e54fc9dc2375bc692eeb6e68350bb42bfc
DevBoiAgru <79085233+DevBoiAgru@users.noreply.github.com>
2025-07-19 17:21:05 +0530

    Improve logging and optimize Redis likes retrieval
    Improved error logging for better traceability, removed unnecessary print statements, and replaced sequential Redis GETs with a single MGET in GetSurroundingLikes for efficiency. Also added some protections.

--- api/numberlike.go
+++ api/numberlike.go
@@ -1,3 +1,7 @@
+// This backend would look and perform wayyyy nicer if I didn't have to rely on vercel
+// for hosting the api. I'd self host it but unfortunately cloudflare requires a domain
+// and I'm not in the mood for getting ddos-ed by a random child.
+
 package handler
 
 import (
@@ -30,22 +34,26 @@ func NumberLike(w http.ResponseWriter, r *http.Request) {
 
 func handlePost(w http.ResponseWriter, r *http.Request) {
 
-	numRaw, err := io.ReadAll(r.Body)
-	defer r.Body.Close()
+	// Use a limited reader so that someone doesnt send a 50TB payload and we load all of
+	// that in memory with io.ReadAll
+	const maxBodySize = 32 // Max 32 bytes
+	limitedReader := io.LimitReader(r.Body, maxBodySize)
+	numRaw, err := io.ReadAll(limitedReader)
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Printf("[ERROR] Error while reading request body: %s\n", err)
 		return
 	}
+	defer r.Body.Close()
 
 	num, err := strconv.ParseInt(string(numRaw), 10, 64)
 	if err != nil {
 		http.Error(w, "Invalid number", http.StatusBadRequest)
-		log.Println("[WARN] Invalid number inputted (string failed to convert to integer)")
+		log.Printf("[WARN] Invalid number inputted: %s\n", numRaw)
 		return
 	}
 
 	addLike(num)
-	log.Printf("[INFO] Liked number %d\n", num)
 
 	// Get number of likes for surrounding numbers to return
 	surrLikes := GetSurroundingLikes(num)
@@ -57,8 +65,7 @@ func handlePost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Printf("%s\n", string(likesJSON))
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusCreated)
 	w.Write(likesJSON)
 }
 
@@ -93,7 +100,7 @@ func handleGet(w http.ResponseWriter, r *http.Request) {
 func handleOptions(w http.ResponseWriter, _ *http.Request) {
 	// Set CORS headers
 
-	// Let vercel handle origins. 
+	// Let vercel handle origins.
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Access-Control-Max-Age", "86400") // 1 day
@@ -122,25 +129,16 @@ func addLike(number int64) {
 	key := fmt.Sprintf("like:%d", number)
 	err := redisClient.Incr(ctx, key).Err()
 	if err != nil {
-		log.Println("Redis Incr error:", err)
+		log.Printf("[ERROR] [REDIS] Error while incrementing likes for %d: %v\n", number, err)
 	}
-}
-
-func getLikes(number int64) uint32 {
-	key := fmt.Sprintf("like:%d", number)
-	val, err := redisClient.Get(ctx, key).Uint64()
-	if err == redis.Nil {
-		return 0
-	} else if err != nil {
-		log.Println("Redis Get error:", err)
-		return 0
-	}
-	return uint32(val)
 }
 
 func GetSurroundingLikes(center int64) map[int64]uint32 {
 	result := make(map[int64]uint32)
 	const radius int64 = 10
+
+	reqNums := make([]string, 0, 21) // 10 above, 10 below, 1 middle
+	nums := make([]int64, 0, 21)     // For keeping track of which number we are getting the number of likes for
 
 	for i := int64(-radius); i <= radius; i++ {
 		n := center + i
@@ -148,7 +146,37 @@ func GetSurroundingLikes(center int64) map[int64]uint32 {
 			// Overflow / underflow happened, skip
 			continue
 		}
-		result[n] = getLikes(n)
+		reqNums = append(reqNums, fmt.Sprintf("like:%d", n))
+		nums = append(nums, n)
+	}
+
+	// Get number of likes for all required numbers at once
+	likes := redisClient.MGet(ctx, reqNums...)
+
+	res, err := likes.Result()
+	if err != nil {
+		log.Printf("[ERROR] [REDIS] Error while getting likes around %d: %v\n", center, err)
+	}
+	for i, val := range res {
+		if val == nil {
+			result[nums[i]] = 0
+		} else {
+
+			// Parse value into a uint32
+			strVal, ok := val.(string)
+			if !ok {
+				log.Printf("[ERROR] [REDIS] Got unexpected type %s\n", val)
+				continue
+			}
+
+			// Convert that string into a number
+			likeNum, err := strconv.ParseInt(strVal, 10, 32)
+			if err != nil {
+				log.Printf("Key %s has non-numeric value: %s\n", reqNums[i], strVal)
+				continue
+			}
+			result[nums[i]] = uint32(likeNum)
+		}
 	}
 
 	return result
